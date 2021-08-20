7a5acb001fa00e5f0e402c9539ea76fc463b21bd
Bruno Souza <bruno.arubesu@gmail.com>
2021-08-20 15:14:04 -0300

    feat(create and list specification): add  specification routes

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+import { SpecificationRepository } from '../modules/repositories/Specification/SpecificationRepository';
+import { CreateSpecificationService } from '../modules/services/Specification/createSpecificationService';
+
+const specificationRoutes = Router();
+
+const repository = new SpecificationRepository();
+const createSpecificationService = new CreateSpecificationService(repository);
+
+specificationRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+
+  createSpecificationService.execute({ name, description });
+
+  return response.status(201).send();
+});
+
+specificationRoutes.get('/', (request, response) => {
+  const specifications = repository.list();
+
+  return response.json(specifications);
+});
+
+export { specificationRoutes };
