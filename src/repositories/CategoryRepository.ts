a8a5dd201230e16a61bce3fae382784193d310a4
Bruno Souza <bruno.arubesu@gmail.com>
2021-08-20 12:44:29 -0300

    refactor(add category): add category repositore create method

---
+++ src/repositories/CategoryRepository.ts
@@ -0,0 +1,33 @@
+import { Category } from "../models/Category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoryRepository {
+
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO) {
+    const category = new Category();
+
+    Object.assign(
+      category,
+      {
+        name,
+        description,
+        createdAt: new Date(),
+      });
+
+    this.categories.push(category);
+  }
+}
+
+export {
+  CategoryRepository
+}
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,26 +1,19 @@
 import { Router } from 'express'
-import { Category } from '../models/Category';
+import { CategoryRepository } from '../repositories/CategoryRepository';
 
 const categoriesRoutes = Router();
-
-const categories = [];
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  const repository = new CategoryRepository();
 
-  Object.assign(
-    category,
-    {
-      name,
-      description,
-      createdAt: new Date(),
-    });
+  repository.create({
+    name,
+    description,
+  });
 
-  categories.push();
-
-  return response.status(201).json(category);
+  return response.status(201).send();
 })
 
 export { categoriesRoutes }
