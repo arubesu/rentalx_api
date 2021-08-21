f38d68829f0d5bd57bc895281abc9aaa0cc5b49d
Bruno Souza <bruno.arubesu@gmail.com>
2021-08-21 18:57:57 -0300

    chore: register specificationRepository in DI container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,15 @@ import { container } from 'tsyringe';
 
 import { CategoryRepository } from '../../modules/cars/repositories/Category/CategoryRepository';
 import { ICategoryRepository } from '../../modules/cars/repositories/Category/ICategoryRepository';
+import { ISpecificationRepository } from '../../modules/cars/repositories/Specification/ISpecificationRepository';
+import { SpecificationRepository } from '../../modules/cars/repositories/Specification/SpecificationRepository';
 
 container.registerSingleton<ICategoryRepository>(
   'CategoryRepository',
   CategoryRepository,
+);
+
+container.registerSingleton<ISpecificationRepository>(
+  'SpecificationRepository',
+  SpecificationRepository,
 );
