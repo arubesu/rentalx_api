05f75121819009bf213cec095bb0b8291b93d5b3
Bruno Souza <bruno.arubesu@gmail.com>
2021-08-23 14:09:02 -0300

    feat(avatar): add users avatar migrations

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateSpecificationController } from '../modules/cars/useCases/Specification/createSpecification/createSpecificationController';
 import { ListSpecificationController } from '../modules/cars/useCases/Specification/listSpecification/listSpecificationController';
 
@@ -7,6 +8,8 @@ const specificationRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationController = new ListSpecificationController();
+
+specificationRoutes.use(ensureAuthenticated);
 
 specificationRoutes.post('/', createSpecificationController.handle);
 
