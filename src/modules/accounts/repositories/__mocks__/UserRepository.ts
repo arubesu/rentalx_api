d659baf5d6ee9ab476226b194d224e3ee502e857
Bruno Souza <bruno.arubesu@gmail.com>
2021-08-23 20:09:49 -0300

    test(authenticate user): add mock userRepository

---
+++ src/modules/accounts/repositories/__mocks__/UserRepository.ts
@@ -0,0 +1,41 @@
+import { User } from '../../entities/User';
+import { IUserRepository, ICreateUserDTO } from '../IUserRepository';
+
+class UserRepository implements IUserRepository {
+  private repository: User[];
+
+  constructor() {
+    this.repository = [];
+  }
+
+  async create({
+    name,
+    password,
+    email,
+    driver_license,
+    id,
+    avatar_url,
+  }: ICreateUserDTO): Promise<void> {
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      password,
+      email,
+      driver_license,
+      avatar_url,
+    });
+
+    this.repository.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.repository.find(user => user.email === email);
+  }
+
+  async findById(userId: string): Promise<User> {
+    return this.repository.find(user => user.id === userId);
+  }
+}
+
+export { UserRepository };
