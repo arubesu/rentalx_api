0dcadc47393f22bd38db6fca29061f98bab94987
Bruno Souza <bruno.arubesu@gmail.com>
2021-08-24 13:01:07 -0300

    chore: log err

--- src/middlewares/exceptionHandler.ts
+++ src/middlewares/exceptionHandler.ts
@@ -13,6 +13,9 @@ export async function exceptionHandler(
       message: err.message,
     });
   }
+
+  console.log(err);
+
   return response.status(500).json({
     status: 'error',
     message: 'Internal server error',
