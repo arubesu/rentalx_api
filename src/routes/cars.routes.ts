f12b23ec08da84be89e26dba33108af0e6b91be6
Bruno Souza <bruno.arubesu@gmail.com>
2021-08-27 16:05:52 -0300

    feat(create car specification): add route

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -3,18 +3,27 @@ import { Router } from 'express';
 import { ensureAuthenticated } from '@middlewares/ensureAuthenticated';
 import { ensureUserIsAdmin } from '@middlewares/ensureUserIsAdmin';
 import { CreateCarController } from '@modules/cars/useCases/Car/createCar/createCarController';
+import { CreateCarSpecificationController } from '@modules/cars/useCases/Car/createCarSpecifications/createCarSpecificationsController';
 import { ListAvailableCarsController } from '@modules/cars/useCases/Car/listAvailableCars/listAvailableCarsController';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
 carsRoutes.post(
   '/',
   ensureAuthenticated,
   ensureUserIsAdmin,
   createCarController.handle,
+);
+
+carsRoutes.post(
+  '/:id/specifications',
+  ensureAuthenticated,
+  ensureUserIsAdmin,
+  createCarSpecificationController.handle,
 );
 
 carsRoutes.get('/', listAvailableCarsController.handle);
