73ee847f7c52aea2c6b492b97b1730445469bd66
Bruno Souza <bruno.arubesu@gmail.com>
2021-08-27 13:28:35 -0300

    feat(list available cars): add get route

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 
+import { ensureAuthenticated } from '@middlewares/ensureAuthenticated';
 import { ensureUserIsAdmin } from '@middlewares/ensureUserIsAdmin';
 import { CreateCarController } from '@modules/cars/useCases/Car/createCar/createCarController';
-
-import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import { ListAvailableCarsController } from '@modules/cars/useCases/Car/listAvailableCars/listAvailableCarsController';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
 carsRoutes.post(
   '/',
@@ -15,5 +16,7 @@ carsRoutes.post(
   ensureUserIsAdmin,
   createCarController.handle,
 );
+
+carsRoutes.get('/', listAvailableCarsController.handle);
 
 export { carsRoutes };
