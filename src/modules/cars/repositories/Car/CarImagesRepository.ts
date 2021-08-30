e2650bc201f70613f23edf73d88fe17dc4ba98a7
Bruno Souza <bruno.arubesu@gmail.com>
2021-08-30 20:15:45 -0300

    feat(add cars images): add repository

---
+++ src/modules/cars/repositories/Car/CarImagesRepository.ts
@@ -0,0 +1,26 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { CarImage } from '@modules/cars/entities/CarImage';
+
+import ICarImagesRepository from './ICarImagesRepository';
+
+class CarImagesRepository implements ICarImagesRepository {
+  private repository: Repository<CarImage>;
+
+  constructor() {
+    this.repository = getRepository(CarImage);
+  }
+
+  async create(car_id: string, image_name: string): Promise<CarImage> {
+    const carImage = this.repository.create({
+      car_id,
+      image_name,
+    });
+
+    await this.repository.save(carImage);
+
+    return carImage;
+  }
+}
+
+export { CarImagesRepository };
