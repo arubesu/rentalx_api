3d5c2cc66aa98ca6c20ec1955a07dd660d4721dd
Bruno Souza <bruno.arubesu@gmail.com>
2021-08-31 18:59:47 -0300

    feat(cars rentals): add rentals repo

---
+++ src/modules/rentals/entities/repositories/IRentalsRepository.ts
@@ -0,0 +1,18 @@
+import { Rental } from '../Rental';
+
+export interface ICreateRentalDTO {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+  id?: string;
+  end_date?: Date;
+  total?: number;
+}
+
+export interface IRentalsRepository {
+  findOpenRentalByCar(car_id: string): Promise<Rental>;
+  findOpenRentalByUser(user_id: string): Promise<Rental>;
+  create(data: ICreateRentalDTO): Promise<Rental>;
+  findById(id: string): Promise<Rental>;
+  findByUser(user_id: string): Promise<Rental[]>;
+}
