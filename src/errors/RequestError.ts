4eededc2808ff0d3a2d9be42e5c6518e7dfce507
Bruno Souza <bruno.arubesu@gmail.com>
2021-09-10 15:24:10 -0300

    chore: fix request error props

--- src/errors/RequestError.ts
+++ src/errors/RequestError.ts
@@ -1,6 +1,6 @@
 class RequestError {
-  private readonly message: string;
-  private readonly statusCode: number;
+  readonly message: string;
+  readonly statusCode: number;
 
   constructor(message: string, statusCode = 400) {
     this.message = message;
