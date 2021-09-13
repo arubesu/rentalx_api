5602f1514bb7da1e5e3defdc001c009589c6b9d5
Bruno Souza <bruno.arubesu@gmail.com>
2021-09-13 11:49:00 -0300

    feat(list rental by user): add controller | use case and route

--- src/routes/rentals.routes.ts
+++ src/routes/rentals.routes.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 
 import { ensureAuthenticated } from '@middlewares/ensureAuthenticated';
 import { CreateRentalController } from '@modules/rentals/useCases/createRentals/createRentalsController';
+import { ListRentalsByUserController } from '@modules/rentals/useCases/listRentalsByUser/listRentalsByUserController';
 import { ReturnRentalController } from '@modules/rentals/useCases/returnRental/returnRentalController';
 
 const rentalsRoutes = Router();
 
 const createRentalUseCase = new CreateRentalController();
 const returnRentalUseCase = new ReturnRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalsRoutes.post('/', ensureAuthenticated, createRentalUseCase.handle);
 
@@ -15,6 +17,12 @@ rentalsRoutes.post(
   '/return/:id',
   ensureAuthenticated,
   returnRentalUseCase.handle,
+);
+
+rentalsRoutes.get(
+  '/user',
+  ensureAuthenticated,
+  listRentalsByUserController.handle,
 );
 
 export { rentalsRoutes };
