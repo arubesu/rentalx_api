f0d2f7691952491b6a8e5602aee052a26fdc4b1f
Bruno Souza <bruno.arubesu@gmail.com>
2021-09-13 11:26:08 -0300

    feat(return rental): add route

--- src/routes/rentals.routes.ts
+++ src/routes/rentals.routes.ts
@@ -2,11 +2,19 @@ import { Router } from 'express';
 
 import { ensureAuthenticated } from '@middlewares/ensureAuthenticated';
 import { CreateRentalController } from '@modules/rentals/useCases/createRentals/createRentalsController';
+import { ReturnRentalController } from '@modules/rentals/useCases/returnRental/returnRentalController';
 
 const rentalsRoutes = Router();
 
 const createRentalUseCase = new CreateRentalController();
+const returnRentalUseCase = new ReturnRentalController();
 
 rentalsRoutes.post('/', ensureAuthenticated, createRentalUseCase.handle);
+
+rentalsRoutes.post(
+  '/return/:id',
+  ensureAuthenticated,
+  returnRentalUseCase.handle,
+);
 
 export { rentalsRoutes };
