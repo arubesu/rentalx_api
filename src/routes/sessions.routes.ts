fbaf27b1468addc496c29d4cc8544e5ae982a16c
Bruno Souza <bruno.arubesu@gmail.com>
2021-09-14 15:31:48 -0300

    feat(refresh token): add controller and route

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 
 import { AuthenticateUserController } from '@modules/accounts/useCases/authenticateUser/AuthenticateUserController';
+import { RefreshTokenController } from '@modules/accounts/useCases/refreshToken/refreshTokenController';
 
 const sessionRoutes = Router();
+
 const sessionsController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
+
 sessionRoutes.post('/', sessionsController.handle);
+sessionRoutes.post('/refresh_token', refreshTokenController.handle);
 
 export { sessionRoutes };
