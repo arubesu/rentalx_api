bb08cc001b7001f84734ce65ea9dca81f6a34926
Bruno Souza <bruno.arubesu@gmail.com>
2021-09-14 15:23:22 -0300

    feat(refresh token): add findByUserIdAndRefreshToken and deleteById in UsersTokensRepository

--- src/modules/accounts/repositories/userTokens/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/userTokens/IUsersTokensRepository.ts
@@ -12,4 +12,11 @@ export interface IUsersTokensRepository {
     refresh_token,
     expires_date,
   }: ICreateUserTokenDTO): Promise<UserTokens>;
+
+  findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string,
+  ): Promise<UserTokens>;
+
+  deleteById(id: string): Promise<void>;
 }
