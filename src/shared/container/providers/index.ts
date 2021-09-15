5ad3d56df60d9e912d7d95f0a17cd94d74c57d3d
Bruno Souza <bruno.arubesu@gmail.com>
2021-09-15 17:41:06 -0300

    chore: add mail provider interface and register mailprovider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,8 +2,15 @@ import { container } from 'tsyringe';
 
 import { DayjsDateProvider } from './dateProvider/dayjsDateProvider';
 import { IDateProvider } from './dateProvider/IDateProvider';
+import { IMailProvider } from './mailProvider/IMailProvider';
+import { EtherealMailProvider } from './mailProvider/implementations/etherealMailProvider';
 
 container.registerSingleton<IDateProvider>(
   'DayjsDateProvider',
   DayjsDateProvider,
+);
+
+container.registerInstance<IMailProvider>(
+  'EtherealMailProvider',
+  new EtherealMailProvider(),
 );
