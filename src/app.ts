7c112d13c2d45ac252e2ea30c5e3a0d3c215ed22
Bruno Souza <bruno.arubesu@gmail.com>
2021-09-22 14:22:56 -0300

    chore: serve static folders

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,8 @@ import { exceptionHandler } from '@middlewares/exceptionHandler';
 import './database';
 import '@shared/container';
 import '@shared/container/providers';
+import upload from '@config/upload';
+
 import { createConnection } from 'typeorm';
 
 import { router } from './routes';
@@ -21,6 +23,10 @@ const app = express();
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
+app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
+
 app.use(router);
 app.use(exceptionHandler);
 
