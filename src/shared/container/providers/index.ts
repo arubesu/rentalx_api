b8227c5df8b0ccea97ccb2cc7e08e3c451af401c
Bruno Souza <bruno.arubesu@gmail.com>
2021-09-22 11:48:28 -0300

    chore: add local storage provider and register in container

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,6 +4,8 @@ import { DayjsDateProvider } from './dateProvider/dayjsDateProvider';
 import { IDateProvider } from './dateProvider/IDateProvider';
 import { IMailProvider } from './mailProvider/IMailProvider';
 import { EtherealMailProvider } from './mailProvider/implementations/etherealMailProvider';
+import { LocalStorageProvider } from './Storage/implementations/localStorageProvider';
+import { IStorageProvider } from './Storage/IStorageProvider';
 
 container.registerSingleton<IDateProvider>(
   'DayjsDateProvider',
@@ -13,4 +15,9 @@ container.registerSingleton<IDateProvider>(
 container.registerInstance<IMailProvider>(
   'EtherealMailProvider',
   new EtherealMailProvider(),
+);
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  LocalStorageProvider,
 );
